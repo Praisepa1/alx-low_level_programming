d236c4564c8974a843d20dfff22c2718fe7d959a
Praisepa1 <tochukwupatrick2003@gmail.com>
2022-09-08 22:59:23 -0700

    name

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
-#include <studio.h>
-
-/**
-*main - print the string in the put function
-*
-*Description: using the main function
-*this program prints "programming is like building a multlingual puzzle
-*Return: 0
+#!/bin/bash                                                                                                                             
+#include <studio.h>                                                                                                                     
+                                                                                                                                        
+/**                                                                                                                                     
+* main - print the string in the put function                                                                                           
+*                                                                                                                                       
+* Description: using the main function                                                                                                  
+* this program prints "programming is like building a multilingual puzzle                                                               
+* Return: 0
 */
 int main(void)
 {
-        puts("\"programming is like building a multilingual puzzle");
-        return (0);
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0);
 }
